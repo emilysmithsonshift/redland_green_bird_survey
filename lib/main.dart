80a3f56f1a4e9e2e053487e53f5f3882aa954f49
Emly <emily_foulkes@hotmail.com>
2021-12-31 10:40:55 +0000

    firebase update

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'firebase_options.dart';
 import 'models/version.dart';
 import 'pages/home_page/home_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
