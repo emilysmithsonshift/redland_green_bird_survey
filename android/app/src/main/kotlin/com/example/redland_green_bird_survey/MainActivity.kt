a976b48622db72520187286a946677d4b3139f75
Emly <emily_foulkes@hotmail.com>
2022-03-05 10:58:22 +0000

    fix gradle

--- android/app/src/main/kotlin/com/example/redland_green_bird_survey/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/redland_green_bird_survey/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.redland_green_bird_survey
 
-+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
